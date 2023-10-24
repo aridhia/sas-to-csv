c80f3fb7f654a5ce3ce9985a5dcfb15cf00401d5
bert <bert@Ubuntu.myguest.virtualbox.org>
2023-10-24 16:09:00 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# sas-to-csv
-Script to convert SAS files to CSV for upload into FAIR
+# SAS to CSV
+
+Script to convert SAS files to CSV files for upload into FAIR.
+
+## Usage
+
+* In the `sas_to_csv.r` script, update the value of `file` to the path to the SAS file that has to be converted.
+* Run the script.
+*  This generates two CSV files, in the same directory as the SAS file.
+    * `<your_SAS_file_name>_data.csv`, containing the actual data, which can be uploaded to FAIR.
+    * `<your_SAS_file_name>_metadata.csv`, containing metadata (name, label, type), which can be used to create a FAIR dictionary for the file.
+
+## Notes
+
+* This script is specifically written for SAS files in SDTM format.
+* It can be used for both `.sas7bdat` and `.xpt` files.
+* It is assumed that the names of all fields in the SAS file that contain dates end in `DTC`. These are assigned in the metadata file the type `date`.
+* Fields of the type `character` in the SAS file are assigned in the metadata file the type `text`.
+* Fields of the type `double` in the SAS file are assigned in the metadata file the type `decimal`.
+
+
---
+++ sas_to_csv.r
@@ -0,0 +1,41 @@
+# Install required packages, if not already installed
+if (!require("data.table")){install.packages("data.table")}
+if (!require("haven")){install.packages("haven")}
+if (!require("labelled")){install.packages("labelled")}
+if (!require("tidyverse")){install.packages("tidyverse")}
+if (!require("tools")){install.packages("tools")}
+
+# Load packages
+library(data.table)
+library(haven)
+library(labelled)
+library(tidyverse)
+library(tools)
+
+# Path to SAS file to be converted
+file = "WIP/sas_to_csv/test.sas7bdat"
+
+base = file_path_sans_ext(file)
+extension = file_ext(file)
+
+# Read in SAS file
+if (extension == "sasb7dat") {
+  df <- read_sas(file)
+} else if (extension == "xpt") {
+  df <- read_xpt(file)
+}
+
+# Select metadata columns
+metadata <- df %>% look_for() %>% select(variable, label, col_type)
+# Rename columns
+metadata <- metadata %>% rename(name = variable, type = col_type)
+# Change type chr to text and dbl to decimal
+metadata <- metadata %>% mutate(type = replace(type, type == "chr", "text"))
+metadata <- metadata %>% mutate(type = replace(type, type == "dbl", "decimal"))
+# Change type of columns all ending on "DTC" to date
+# This assumes that all date columns in SDTM (and ADaM?) end on "DTC"
+metadata <- metadata %>% mutate(type = replace(type, name %like% ("DTC$"), "date"))
+
+# Write data and metadata to CSV files
+write.csv(df, paste(base, "_data.csv"), row.names=FALSE)
+write.csv(metadata, paste(base, "_metadata.csv"), row.names=FALSE)
